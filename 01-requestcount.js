ad46142b07608900148050c42db0b9098e25d895
RAVIRANJAN95 <ravicrook.9@gmail.com>
2026-01-16 20:25:53 +0530

    add week 3 middleware assignments -all questions

--- 01-requestcount.js
+++ 01-requestcount.js
@@ -1,34 +1,40 @@
 const request = require('supertest');
 const assert = require('assert');
+
 const express = require('express');
-
 const app = express();
-let requestCount = 0;
-
 // You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of requests made to the server in the global
-// requestCount variable
-app.use((req,res,next) =>{
-  requestCount++;
-  next();
+//@@ -11,11 +10,29 @@ const app = express();
+// You have been given a numberOfRequestsForUser object to start off with which
+// clears every one second
+
+
+let numberOfRequestsForUser = {};
+setInterval(() => {
+    numberOfRequestsForUser = {};
+}, 1000)
+
+app.use((req,res,next)=>{
+  // numberOfRequestsForUser[req.headers["user-id"]]++;
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("no entry");
+    }
+    else{
+      next();
+          }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
 })
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
+})
+const PORT =3000;
+app.listen(PORT ,() =>{
+  console.log('server is running on port $ {PORT}');
 });
-
-
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
-});
-
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
-});
-
-app.get('/requestCount', function(req, res) {
-  res.status(200).json({ requestCount });
-});
-
-module.exports = app;
